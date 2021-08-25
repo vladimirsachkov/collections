b7fb4e3a4e144371b3e7f159781190bd35aa73a6
alexa <alexanderredar1@gmail.com>
2021-08-25 20:48:16 +0300

    1) Одномерный массив

---
+++ src/com/javacode/arrays/ArrayLauncher.java
@@ -0,0 +1,20 @@
+package com.javacode.arrays;
+
+import java.util.Scanner;
+
+public class ArrayLauncher {
+
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] myArray = new int[8];
+        System.out.println("Please enter 8 int elements");
+        for (int j=0; j < myArray.length; j++) {
+            System.out.println("Next element");
+            myArray[j] = scanner.nextInt();
+        }
+
+        for (int i=0; i < myArray.length; i++) {
+            System.out.println("Element #"+ i + " = "+myArray[i]);
+        }
+    }
+}
--- src/com/javacode/arrays/test.java
+++
@@ -1,4 +0,0 @@
-package com.javacode.arrays;
-
-public class test {
-}
