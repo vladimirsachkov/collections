dfbd7f873f2c2f5828f963ddbbc06e7641c5892b
alexa <alexanderredar1@gmail.com>
2021-09-13 21:49:21 +0300

    18) Методы equals и hashCode.

--- src/com/javacode/set/SetRunner.java
+++ src/com/javacode/set/SetRunner.java
@@ -6,20 +6,34 @@ public class SetRunner {
 
     public static void main(String[] args) {
 
-        String[] cars = {"Mini", "Mercedes-Benz", "Audi" , "VM", "Smart", "Toyota" , "Porsche"};
-        String[] otherCars = {"Audi", "Ford", "GMC", "Toyota", "Chevrolet"};
+        Set<Car> sixCars = new HashSet<>();
+        sixCars.add(new Car("VW", "Golf", 35));
+        sixCars.add(new Car("Audi", "A3", 60));
+        sixCars.add(new Car("VW", "Polo", 25));
+        sixCars.add(new Car("BMW", "Z4", 120));
+        sixCars.add(new Car("BMW", "440i", 200));
 
-        Set<String> carSet = new TreeSet<>(Arrays.asList(cars));
 
-        Set<String> otherCarSet = new TreeSet<>(Arrays.asList(otherCars));
+        Set<Car> europaCars = new HashSet<>();
+        europaCars.add(new Car("Toyota", "Auris", 40));
+        europaCars.add(new Car("Reno", "Clio", 30));
+        europaCars.add(new Car("Reno", "Megan", 50));
+        europaCars.add(new Car("VW", "Golf", 45));
+        europaCars.add(new Car("VW", "Polo", 35));
 
-        Set<String> uniqueCars = new TreeSet<>(carSet);
-        uniqueCars.addAll(otherCarSet);
-
+        Set<Car> uniqueCars = new HashSet<>(sixCars);
+        uniqueCars.addAll(europaCars);
         print(uniqueCars);
+
+        if (sixCars==europaCars) {
+
+        }
     }
 
-    private static void  print(Set<String> cars) {
-        System.out.println(cars);
+    private static void  print(Set<Car> cars) {
+        System.out.printf("%-20s %-20s %-20s \n", "Cars brand", "Model", "Price per day");
+        for (Car car: cars) {
+            System.out.printf("%-20s %-20s %-20s \n", car.getCarBrand(), car.getModel(), car.getPricePerDay());
+        }
     }
 }
