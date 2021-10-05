d1342eaebcbdddc348afb13d65c27e6da7394ec4
alexa <alexanderredar1@gmail.com>
2021-10-05 20:29:05 +0300

    23) Класс HashMap. Часть 1.

--- src/com/javacode/map/MapLauncher.java
+++ src/com/javacode/map/MapLauncher.java
@@ -1,18 +1,38 @@
 package com.javacode.map;
 
-import com.javacode.set.Car;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MapLauncher {
 
     public static void main(String[] args) {
-        Map<String, Integer> numbers = new HashMap<>();
-        numbers.put("Toyota", 50);
-        numbers.put("Audi", 51);
-        System.out.println(numbers.put("Toyota", 52));
+        Map<String, Integer> wordMap = new HashMap<>();
+        System.out.println("Please enter some text");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        String[] tokens = string.split(" ");
+        for (String token : tokens) {
+            String word = token.toLowerCase();
+            Integer count = wordMap.get(word);
+            if (count == null) {
+                wordMap.put(word, 1);
+            } else {
+                wordMap.put(word, count + 1);
+            }
+        }
 
-//        System.out.println(numbers.get(new Car("Audi", "A5", 50)));
+        NavigableSet<WordWrapper> wordWrappers = convertToSet(wordMap);
+        printSet(wordWrappers);
+    }
+
+    private static void printSet(NavigableSet<WordWrapper> wordWrappers) {
+
+    }
+
+    private static NavigableSet<WordWrapper> convertToSet (Map<String, Integer> wordMap) {
+        NavigableSet<WordWrapper> wordSet = new TreeSet<>();
+        for (Map.Entry<String, Integer> e : wordMap.entrySet()) {
+            wordSet.add(new WordWrapper(e.getKey(), e.getValue()));
+        }
+        return wordSet;
     }
 }
